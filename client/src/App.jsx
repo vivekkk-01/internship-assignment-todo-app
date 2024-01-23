eb0c4422f770a8d4d06968e530f051a20921c91f
vivek <chimnanivivek14@gmail.com>
2024-01-23 18:33:22 +0530

    created all tasks page in frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,19 @@ import ResetPassword from "./pages/ResetPassword";
 import AskForEmail from "./pages/AskForEmail";
 import ResetPasswordFromEmail from "./pages/ResetPasswordFromEmail";
 import Register from "./pages/Register";
+import AllTasks from "./pages/AllTasks";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/login", element: <Login /> },
-  {path: "/register", element: <Register/>},
+  { path: "/register", element: <Register /> },
   { path: "/reset-password", element: <ResetPassword /> },
   { path: "/ask-for-email", element: <AskForEmail /> },
   {
     path: "/reset-password-from-email/:token",
     element: <ResetPasswordFromEmail />,
   },
+  { path: "/all", element: <AllTasks /> },
 ]);
 
 function App() {
