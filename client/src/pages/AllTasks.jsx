f59b948887ec2094d643f4b489d914bcde0bacd5
vivek <chimnanivivek14@gmail.com>
2024-01-24 19:26:04 +0530

    sort and filter logic in frontend

--- client/src/pages/AllTasks.jsx
+++ client/src/pages/AllTasks.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllTasksAction } from "../redux/actions/task";
 import { ClipLoader } from "react-spinners";
 import Task from "../components/Task";
 import { IoIosAdd } from "react-icons/io";
 import CreateTaskModal from "../modals/CreateTaskModal";
+import { RiSoundModuleLine } from "react-icons/ri";
+import { BiSort } from "react-icons/bi";
+import useClickOutside from "../hooks/useClickOutside";
+import categories from "../utils/categories";
 
 const AllTasks = () => {
   const [addTask, setAddTask] = useState(false);
@@ -12,10 +16,40 @@ const AllTasks = () => {
   const { allTasksLoading, tasks, allTasksError } = useSelector(
     (state) => state.task
   );
+  const [isFilter, setIsFilter] = useState(false);
+  const [selectedFilter, setSelectedFilter] = useState("");
+  const filterRef = useRef();
+
+  const [isSort, setIsSort] = useState(false);
+  const [selectedSort, setSelectedSort] = useState("");
+  const sortRef = useRef();
+
+  useClickOutside(filterRef, () => setIsFilter(false));
+  useClickOutside(sortRef, () => setIsSort(false));
 
   useEffect(() => {
     dispatch(getAllTasksAction());
   }, []);
+
+  const handleFilter = (category) => {
+    if (category == selectedSort) {
+      setSelectedFilter("");
+      //TODO
+    } else {
+      setSelectedFilter(category);
+      // TODO
+    }
+  };
+
+  const handleSort = (sort) => {
+    if (sort === selectedSort) {
+      setSelectedSort("");
+      // TODO
+    } else {
+      setSelectedSort(sort);
+      // TODO
+    }
+  };
 
   if (tasks.length <= 0) {
     return (
@@ -46,22 +80,83 @@ const AllTasks = () => {
           <h1 className="text-red-700 font-bold text-3xl">{allTasksError}</h1>
         </div>
       ) : (
-        <div className="w-full h-full grid grid-cols-4 gap-4 bg-red-100 py-4 px-6 overflow-y-scroll">
-          {tasks.map((task) => {
-            return (
-              <Task
-                key={task.id}
-                id={task.id}
-                status={task.status}
-                title={task.title}
-                description={task.description}
-                category={task.category}
-                image={task?.image}
-                startDate={task.startDate}
-                endDate={task.endDate}
-              />
-            );
-          })}
+        <div className="w-full h-full bg-red-100 py-4 px-6 relative flex flex-col overflow-x-hidden overflow-y-hidden">
+          <div className="m-4 flex items-center gap-3 top-2 right-4  self-end">
+            <div
+              onClick={() => setIsFilter(true)}
+              className={`relative cursor-pointer flex items-center py-2 px-4 border-2 border-gray-400 rounded-xl gap-2`}
+            >
+              <RiSoundModuleLine className="font-semibold" />
+              <span className="font-semibold">Filter</span>
+              {isFilter && (
+                <div
+                  className="bg-white h-44 overflow-y-scroll w-44 shadow-2xl px-2 py-2 z-20 absolute top-12 -left-3 flex flex-col items-start justify-start gap-1"
+                  ref={filterRef}
+                >
+                  {categories.map((category) => {
+                    return (
+                      <p
+                        onClick={handleFilter.bind(null, category)}
+                        key={category}
+                        className={`flex items-center gap-2 w-full hover:bg-gray-600 hover:text-gray-100 hover:rounded-lg ${
+                          selectedFilter === category
+                            ? "bg-gray-600 text-gray-100 rounded-lg"
+                            : ""
+                        } cursor-pointer p-1 px-2`}
+                      >
+                        {category}
+                      </p>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
+            <div
+              onClick={() => setIsSort(true)}
+              className="relative cursor-pointer flex items-center py-2 px-4 border-2 border-gray-400 rounded-xl gap-2"
+            >
+              <BiSort className="font-semibold" />
+              <span className="font-semibold">Sort</span>
+              {isSort && (
+                <div
+                  className="bg-white  w-28 shadow-2xl px-2 py-2 z-20 absolute top-12 right-0 flex flex-col items-start justify-start gap-1"
+                  ref={sortRef}
+                >
+                  {["Newest", "Oldest"].map((sort) => {
+                    return (
+                      <p
+                        onClick={handleSort.bind(null, sort)}
+                        className={`flex items-center gap-2 w-full hover:bg-gray-600 hover:text-gray-100 hover:rounded-lg ${
+                          selectedSort === sort
+                            ? "bg-gray-600 text-gray-100 rounded-lg"
+                            : ""
+                        } cursor-pointer p-1 px-2`}
+                      >
+                        {sort}
+                      </p>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="h-full w-full overflow-y-scroll grid grid-cols-4 gap-4">
+            {tasks.map((task) => {
+              return (
+                <Task
+                  key={task.id}
+                  id={task.id}
+                  status={task.status}
+                  title={task.title}
+                  description={task.description}
+                  category={task.category}
+                  image={task?.image}
+                  startDate={task.startDate}
+                  endDate={task.endDate}
+                />
+              );
+            })}
+          </div>
         </div>
       )}
     </div>
