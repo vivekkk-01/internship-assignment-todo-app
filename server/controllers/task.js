a96df7899d76bcdf0ba35fa333bcf99e928f0b9a
vivek <chimnanivivek14@gmail.com>
2024-01-24 23:03:55 +0530

    fixed a minor issue in getAllTasks controller in backend

--- server/controllers/task.js
+++ server/controllers/task.js
@@ -70,9 +70,7 @@ exports.addTask = async (req, res) => {
 
 exports.getAllTasks = async (req, res) => {
   try {
-    const tasks = await Task.find({ user: req.user.id }).sort({
-      createdAt: -1,
-    });
+    const tasks = await Task.find({ user: req.user.id })
     if (!tasks)
       return res.json("You haven't created any Task. Create your first Task!");
 
