17d9f3ac0863fa735ecdf1dbfc13edda52377163
vivek <chimnanivivek14@gmail.com>
2024-01-25 17:11:59 +0530

    fixed some isuses

--- server/app.js
+++ server/app.js
@@ -5,11 +5,7 @@ const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 8000;
 
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_LINK],
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
