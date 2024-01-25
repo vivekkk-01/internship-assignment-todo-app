7083893b2ef0ba4ad038c174773cca5634b0ea0c
vivek <chimnanivivek14@gmail.com>
2024-01-25 20:21:19 +0530

    edited cors in backend

--- server/app.js
+++ server/app.js
@@ -5,12 +5,7 @@ const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 8000;
 
-app.use(
-  cors({
-    origin: "https://my-todos-g0de.onrender.com",
-  })
-);
-app.options("*", cors());
+app.use(cors());
 
 app.use(express.json());
 
