c32adf5eade3f25b93e54e10dcfe0243c26420dd
vivek <chimnanivivek14@gmail.com>
2024-01-26 14:41:38 +0530

    using dotenv rather than dotenv-safe

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv-safe").config();
+require("dotenv").config();
 const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 8000;
