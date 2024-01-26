203dda510d69bd6682dcb0312f49d313d8daaf47
vivek <chimnanivivek14@gmail.com>
2024-01-26 14:35:32 +0530

    specified cors

--- server/app.js
+++ server/app.js
@@ -5,21 +5,7 @@ const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 8000;
 
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://internship-assignment-todo-jvtfb0n3a-viveks-projects-281df315.vercel.app"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
+app.use(cors());
 
 app.use(express.json());
 
